7fd1ffa8ca010abbb4c781244491b87b712e068b
John Zhang <zxukai@gmail.com>
2016-07-07 17:16:58 +0800

    使用BuildConfig配置API域名

--- app/src/main/java/com/jnhyxx/html5/net/Api.java
+++ app/src/main/java/com/jnhyxx/html5/net/Api.java
@@ -1,22 +1,10 @@
 package com.jnhyxx.html5.net;
 
-import com.jnhyxx.html5.Variant;
+import com.jnhyxx.html5.BuildConfig;
 
 public class Api {
 
-    public static String HOST = "http://www.jnhyxx.com";
-
-    static {
-        if (Variant.isApp1()) {
-            HOST = "http://app1.jnhyxx.com";
-        } else if (Variant.isForH5()) {
-            HOST = "http://test.jnhyxx.com";
-        } else if (Variant.isMicroil()) {
-            HOST = "http://wp7.jnhyxx.com";
-        } else if (Variant.isYcp()) {
-            HOST = "http://ycp.jnhyxx.com";
-        }
-    }
+    public static String HOST = BuildConfig.API_HOST;
 
     //public static final String HOST0 = "http://www.jnhyxx.com";
 

7a49d163e08cb0d3a1dfaa2cc11bc4df1091b01b
John Zhang <zxukai@gmail.com>
2016-07-18 23:34:03 +0800

    修改项目里面的targetSdkVersion为22，由于23新增了权限机制，集成的第三方库不支持

--- app/src/main/java/com/jnhyxx/html5/activity/MainActivity.java
+++ app/src/main/java/com/jnhyxx/html5/activity/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends BaseActivity {
     private boolean mLoadSuccess;
     private String mPageUrl;
 
-
     private static class WebHandler extends Handler {
 
         private Context mContext;

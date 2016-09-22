bb42954edc9583949c9e35a8863862e4113234f9
王杰 <wangjie@hashxy.com>
2016-09-22 16:26:24 +0800

    增加提现记录详情界面的布局

---
+++ app/src/main/java/com/jnhyxx/html5/activity/account/withdraw/WithdrawRecordInfoActivity.java
@@ -0,0 +1,16 @@
+package com.jnhyxx.html5.activity.account.withdraw;
+
+import android.os.Bundle;
+
+import com.jnhyxx.html5.R;
+import com.jnhyxx.html5.activity.BaseActivity;
+
+public class WithdrawRecordInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw_record_info);
+
+    }
+}

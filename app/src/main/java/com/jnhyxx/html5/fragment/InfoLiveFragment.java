f502bcd489a7aa9bf6ccacb1768d58239cf961d2
wangjie <1291360102@qq.com>
2016-10-21 15:05:11 +0800

    修改资讯详情界面算法和布局

--- app/src/main/java/com/jnhyxx/html5/fragment/InfoLiveFragment.java
+++ app/src/main/java/com/jnhyxx/html5/fragment/InfoLiveFragment.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.text.TextUtils;
 import android.util.Log;
@@ -18,10 +19,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.jnhyxx.html5.R;
-import com.jnhyxx.html5.domain.InfoLiveMessage;
 import com.jnhyxx.html5.net.API;
 import com.jnhyxx.html5.net.Callback;
 import com.jnhyxx.html5.net.Resp;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -100,8 +101,8 @@ public class InfoLiveFragment extends BaseFragment implements AbsListView.OnScro
                                  public void onReceive(Resp resp) {
                                      if (resp.isSuccess()) {
 
-                                         Log.d(TAG, "直播资讯" + resp.getData().toString());
-                                         ArrayList<InfoLiveMessage> infoLiveMessageList = new ArrayList<>();
+//                                         Log.d(TAG, "直播资讯" + resp.getData().toString());
+                                         ArrayList<ArrayList<String>> infoLiveMessageList = new ArrayList<>();
                                          String timeHint = "0#1#";
                                          String well = "#";
                                          String endHint = "#####0###";
@@ -109,22 +110,21 @@ public class InfoLiveFragment extends BaseFragment implements AbsListView.OnScro
                                              JSONArray jsonArray = new JSONArray(resp.getData().toString().replaceAll("\\\"", "\""));
                                              for (int i = 0; i < jsonArray.length(); i++) {
                                                  String data = jsonArray.optString(i);
-                                                 Log.d(TAG, "直播具体数据" + data);
-                                                 String time = "";
-                                                 String content = "";
-
+//                                                 Log.d(TAG, "直播具体数据" + data);
                                                  String[] split = data.split(well);
                                                  ArrayList<String> strings = new ArrayList<>();
                                                  for (int j = 0; j < split.length; j++) {
                                                      if (split[j] == null || split[j].length() == 1 || TextUtils.isEmpty(split[j])) {
                                                          continue;
                                                      }
-                                                     Log.d(TAG, "分割开的数据" + split[j].toString());
+//                                                     Log.d(TAG, "分割开的数据" + split[j].toString());
                                                      strings.add(split[j]);
                                                  }
 
-                                                 InfoLiveMessage infoLiveMessage = new InfoLiveMessage(strings.get(0), strings.get(1), null, false);
-                                                 infoLiveMessageList.add(infoLiveMessage);
+//                                                 InfoLiveMessage infoLiveMessage = new InfoLiveMessage(strings.get(0), strings.get(1), null, false);
+                                                 if (!strings.toString().contains("<a") && !strings.toString().contains("</a>")) {
+                                                     infoLiveMessageList.add(strings);
+                                                 }
                                              }
                                          } catch (JSONException e) {
                                              e.printStackTrace();
@@ -142,7 +142,7 @@ public class InfoLiveFragment extends BaseFragment implements AbsListView.OnScro
                 ).fire();
     }
 
-    private void setAdapter(ArrayList<InfoLiveMessage> infoLiveMessageList) {
+    private void setAdapter(ArrayList<ArrayList<String>> infoLiveMessageList) {
         if (infoLiveMessageList == null || infoLiveMessageList.isEmpty()) {
             mListView.setEmptyView(mEmptyView);
             return;
@@ -173,7 +173,7 @@ public class InfoLiveFragment extends BaseFragment implements AbsListView.OnScro
         mSwipeRefreshLayout.setEnabled(firstVisibleItem == 0 && topRowVerticalPosition >= 0);
     }
 
-    class InfoLiveMessageAdapter extends ArrayAdapter<InfoLiveMessage> {
+    class InfoLiveMessageAdapter extends ArrayAdapter<ArrayList<String>> {
 
         Context mContext;
 
@@ -195,40 +195,80 @@ public class InfoLiveFragment extends BaseFragment implements AbsListView.OnScro
                 mViewHolder = (ViewHolder) convertView.getTag();
             }
 
-            InfoLiveMessage infoLiveMessage = getItem(position);
+            ArrayList<String> infoLiveMessage = getItem(position);
+            Log.d(TAG, "大小" + infoLiveMessage.size() + "\n直播的具体数据" + infoLiveMessage.toString());
 
             String[] mSplit = null;
 
             StringBuffer stringBuffer = null;
             if (infoLiveMessage != null) {
+                for (int i = 0; i < infoLiveMessage.size(); i++) {
+                    String content = infoLiveMessage.get(1);
+                    if (content.contains("<b>") || content.contains("</b>")) {
+                        content = content.replace("<b>", "").replaceAll("</b>", "");
+                    } else if (content.contains("<br />")) {
+                        mSplit = content.split("<br />");
+                        stringBuffer = new StringBuffer();
+//                        Log.d(TAG, "含有换行符" + content);
+                        for (String s : mSplit) {
+                            stringBuffer.append(s + "\n");
+                        }
+                        content = stringBuffer.toString().replaceAll("<br />", "");
+                    } else if (content.contains("</br>")) {
+                        mSplit = content.split("</br>");
+                        stringBuffer = new StringBuffer();
+//                        Log.d(TAG, "含有换行符" + content);
+                        for (String s : mSplit) {
+                            stringBuffer.append(s + "\n");
+                        }
+                        content = stringBuffer.toString().replaceAll("</br>", "");
+                    }
+                    if (content.contains("【") && content.contains("】")) {
+                        content = content.substring(content.indexOf("【"));
+                        mViewHolder.mContent.setTextColor(ContextCompat.getColor(getContext(), R.color.redPrimary));
+                    } else {
+                        mViewHolder.mContent.setTextColor(ContextCompat.getColor(getContext(), R.color.blackPrimary));
+                    }
 
-                String content = infoLiveMessage.getContent();
-                if (content.contains("<b>") || content.contains("</b>")) {
-                    content = content.replace("<b>", "").replaceAll("</b>", "");
-                } else if (content.contains("<br />")) {
-                    mSplit = content.split("<br />");
-                    stringBuffer = new StringBuffer();
-                    Log.d(TAG, "含有换行符" + content);
-                    for (String s : mSplit) {
-                        stringBuffer.append(s + "\n");
+                    mViewHolder.mTime.setText(infoLiveMessage.get(0));
+                    mViewHolder.mContent.setText(content);
+
+                    String messageData = infoLiveMessage.toString();
+                    if (messageData.contains(".png") || messageData.contains(".jpg") || messageData.contains(".jpeg")) {
+                        int imageUrlPosition = 0;
+                        for (int j = 0; j < infoLiveMessage.size(); j++) {
+                            String s = infoLiveMessage.get(j);
+                            if (s.contains(".png") || s.contains(".jpg") || s.contains(".jpeg")) {
+                                imageUrlPosition = j;
+                                break;
+                            }
+                        }
+                        mViewHolder.mImageHint.setVisibility(View.VISIBLE);
+                        String imageUrl = "https://res.6006.com/jin10/" + infoLiveMessage.get(imageUrlPosition);
+                        Log.d(TAG, "图片地址" + imageUrl);
+                        Picasso.with(getContext()).load(imageUrl).into(mViewHolder.mImageHint);
+                    } else {
+                        mViewHolder.mImageHint.setVisibility(View.GONE);
                     }
-                    content = stringBuffer.toString().replaceAll("<br />", "");
-                } else if (content.contains("</br>")) {
-                    mSplit = content.split("</br>");
-                    stringBuffer = new StringBuffer();
-                    Log.d(TAG, "含有换行符" + content);
-                    for (String s : mSplit) {
-                        stringBuffer.append(s + "\n");
-                    }
-                    content = stringBuffer.toString().replaceAll("</br>", "");
                 }
-//                else if (content.contains("<a")) {
-//                    mInfoLiveMessageAdapter.remove(getItem(position));
-//                }
 
-                mViewHolder.mTime.setText(infoLiveMessage.getTime());
-                mViewHolder.mContent.setText(content);
+                if (infoLiveMessage.size() == 10) {
+                    Log.d("55555", "size大小" + infoLiveMessage.size() + "   " + infoLiveMessage.toString());
+                    if (!TextUtils.isEmpty(infoLiveMessage.get(2))) {
+                        mViewHolder.mDataLayout.setVisibility(View.VISIBLE);
+                        if (!TextUtils.isEmpty(infoLiveMessage.get(2))) {
+                            mViewHolder.mBeforeData.setText(getString(R.string.expect_data, infoLiveMessage.get(2)));
+                        }
+                        if (!TextUtils.isEmpty(infoLiveMessage.get(3))) {
+                            mViewHolder.mExpectData.setText(getString(R.string.expect_data, infoLiveMessage.get(3)));
+                        }
+                        if (!TextUtils.isEmpty(infoLiveMessage.get(4))) {
+                            mViewHolder.mRealData.setText(getString(R.string.expect_data, infoLiveMessage.get(4)));
+                        }
+                    }
+                }
             }
+
             return convertView;
         }
 

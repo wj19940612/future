3f04cc0eff2642b11d30693a0c6e7530d2dba5df
wangjie <1291360102@qq.com>
2016-10-31 18:16:12 +0800

    删除不用方法

--- app/src/main/java/com/jnhyxx/html5/utils/FontUtil.java
+++ app/src/main/java/com/jnhyxx/html5/utils/FontUtil.java
@@ -1,17 +1,11 @@
 package com.jnhyxx.html5.utils;
 
-import android.content.Context;
 import android.graphics.Typeface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.jnhyxx.html5.App;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 
 public class FontUtil {
 
@@ -41,33 +35,6 @@ public class FontUtil {
             textView.setTypeface(getTt0173MFont());
         }
     }
-
-
-
-    public static   String getAssetsCacheFile(Context context, String fileName) {
-        File cacheFile = new File(context.getCacheDir(), fileName);
-        try {
-            InputStream inputStream = context.getAssets().open(fileName);
-            try {
-                FileOutputStream outputStream = new FileOutputStream(cacheFile);
-                try {
-                    byte[] buf = new byte[1024];
-                    int len;
-                    while ((len = inputStream.read(buf)) > 0) {
-                        outputStream.write(buf, 0, len);
-                    }
-                } finally {
-                    outputStream.close();
-                }
-            } finally {
-                inputStream.close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return cacheFile.getAbsolutePath();
-    }
-
 //    public static void setOpenSans(View view) {
 //        if (view instanceof ViewGroup) {
 //            ViewGroup viewGroup = (ViewGroup) view;

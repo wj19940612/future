c026a4fddc15eec93755b84119b257df3706787b
wangjie <1291360102@qq.com>
2016-11-17 17:32:17 +0800

    在通知弹框的详情界面增加webView

--- app/src/main/java/com/jnhyxx/html5/activity/account/MessageCenterListItemInfoActivity.java
+++ app/src/main/java/com/jnhyxx/html5/activity/account/MessageCenterListItemInfoActivity.java
@@ -2,6 +2,8 @@ package com.jnhyxx.html5.activity.account;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.jnhyxx.html5.R;
@@ -20,6 +22,8 @@ public class MessageCenterListItemInfoActivity extends BaseActivity {
     TextView mTvMessageTime;
     @BindView(R.id.messageContent)
     TextView mTvMessageContent;
+    @BindView(R.id.webView)
+    WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +36,21 @@ public class MessageCenterListItemInfoActivity extends BaseActivity {
 
     private void initData() {
         Intent intent = getIntent();
+        boolean isUpdateNotice = intent.getBooleanExtra(Launcher.EX_PAYLOAD_1, false);
+
         SysMessage mSysMessage = (SysMessage) intent.getSerializableExtra(Launcher.EX_PAYLOAD);
         mTvMessageTitle.setText(mSysMessage.getPushTopic());
         mTvMessageTime.setText(DateUtil.format(mSysMessage.getCreateTime(), DateUtil.DEFAULT_FORMAT, "yyyy/MM/dd HH:mm:ss"));
-        String content = "\t\t\t\t" + mSysMessage.getPushMsg().replaceAll("<p>|</p>", "\r\n");
-        mTvMessageContent.setText(content);
+        if (isUpdateNotice) {
+            mWebView.setVisibility(View.VISIBLE);
+            mWebView.loadDataWithBaseURL(null, mSysMessage.getPushMsg(), "text/html", "utf-8", null);
+            mTvMessageContent.setVisibility(View.GONE);
+        } else {
+            mWebView.setVisibility(View.GONE);
+            mTvMessageContent.setVisibility(View.VISIBLE);
+            String content = "\t\t\t\t" + mSysMessage.getPushMsg().replaceAll("<p>|</p>", "\r\n");
+            mTvMessageContent.setText(content);
+        }
 
     }
 }

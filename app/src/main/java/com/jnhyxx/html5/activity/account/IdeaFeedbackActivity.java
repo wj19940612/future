7fb003f6d97e79fc9dc9b01347c4d68bc23ef54b
wangjie <1291360102@qq.com>
2016-12-02 13:42:19 +0800

    增加意见反馈界面

---
+++ app/src/main/java/com/jnhyxx/html5/activity/account/IdeaFeedbackActivity.java
@@ -0,0 +1,15 @@
+package com.jnhyxx.html5.activity.account;
+
+import android.os.Bundle;
+
+import com.jnhyxx.html5.R;
+import com.jnhyxx.html5.activity.BaseActivity;
+
+public class IdeaFeedbackActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_idea_feedback);
+    }
+}

5ede3c1253f0edbcfc0a41be911175206a1c8a47
wangjie <1291360102@qq.com>
2016-12-02 18:33:46 +0800

    意见反馈界面逻辑和我的界面意见反馈入口

--- app/src/main/java/com/jnhyxx/html5/activity/account/IdeaFeedbackActivity.java
+++ app/src/main/java/com/jnhyxx/html5/activity/account/IdeaFeedbackActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.JsonObject;
@@ -15,6 +17,7 @@ import com.jnhyxx.html5.domain.local.LocalUser;
 import com.jnhyxx.html5.net.API;
 import com.jnhyxx.html5.net.Callback2;
 import com.jnhyxx.html5.net.Resp;
+import com.jnhyxx.html5.utils.KeyBoardHelper;
 import com.jnhyxx.html5.utils.ToastUtil;
 import com.jnhyxx.html5.utils.ValidationWatcher;
 
@@ -33,15 +36,65 @@ public class IdeaFeedbackActivity extends BaseActivity {
     @BindView(R.id.feedbackContentNumber)
     TextView mFeedbackContentNumber;
 
+    @BindView(R.id.topLayout)
+    LinearLayout mTopLayout;
+    @BindView(R.id.emptyLayout)
+    View mHideLayout;
+
+    private KeyBoardHelper mKeyBoardHelper;
+    private int bottomHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idea_feedback);
         ButterKnife.bind(this);
-
+        mFeedbackContentNumber.setText(getString(R.string.feedback_content_number, 0));
         mFeedbackContent.addTextChangedListener(mValidationWatcher);
+        setKeyboardHelper();
     }
 
+    /**
+     * 设置对键盘高度的监听
+     */
+    private void setKeyboardHelper() {
+        mKeyBoardHelper = new KeyBoardHelper(this);
+        mKeyBoardHelper.onCreate();
+//        mKeyBoardHelper.setOnKeyBoardStatusChangeListener(onKeyBoardStatusChangeListener);
+//        mHideLayout.post(new Runnable() {
+//            @Override
+//            public void run() {
+//                bottomHeight = mHideLayout.getHeight();
+//            }
+//        });
+    }
+
+    private KeyBoardHelper.OnKeyBoardStatusChangeListener onKeyBoardStatusChangeListener = new KeyBoardHelper.OnKeyBoardStatusChangeListener() {
+
+        @Override
+        public void OnKeyBoardPop(int keyboardHeight) {
+            final int height = keyboardHeight;
+            if (bottomHeight < height) {
+                int offset = bottomHeight - height;
+                final ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) mTopLayout
+                        .getLayoutParams();
+                lp.topMargin = offset;
+                mTopLayout.setLayoutParams(lp);
+            }
+
+        }
+
+        @Override
+        public void OnKeyBoardClose(int oldKeyboardHeight) {
+            ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) mTopLayout
+                    .getLayoutParams();
+            if (lp.topMargin != 0) {
+                lp.topMargin = 0;
+                mTopLayout.setLayoutParams(lp);
+            }
+
+        }
+    };
     private ValidationWatcher mValidationWatcher = new ValidationWatcher() {
         @Override
         public void afterTextChanged(Editable editable) {
@@ -61,22 +114,19 @@ public class IdeaFeedbackActivity extends BaseActivity {
         String userId = "";
         String userName = "";
         String realName = "";
-        String userPhone;
         if (LocalUser.getUser().isLogin()) {
             UserInfo userInfo = LocalUser.getUser().getUserInfo();
             if (userInfo != null) {
                 userName = userInfo.getUserName();
                 realName = userInfo.getRealName();
-                userPhone = userInfo.getUserPhone();
             }
         }
         API.User.submitFeedBack(mFeedbackContent.getText().toString(), null, userName, realName, mFeedbackConnectWay.getText().toString())
                 .setCallback(new Callback2<Resp<JsonObject>, JsonObject>() {
                     @Override
                     public void onRespSuccess(JsonObject jsonObject) {
-                        if (jsonObject != null) {
-                            ToastUtil.curt(jsonObject.toString());
-                        }
+                        ToastUtil.curt(R.string.feedback_submit_success);
+                        finish();
                     }
                 })
                 .setIndeterminate(this)
@@ -88,6 +138,7 @@ public class IdeaFeedbackActivity extends BaseActivity {
     protected void onDestroy() {
         super.onDestroy();
         mFeedbackContent.removeTextChangedListener(mValidationWatcher);
+        mKeyBoardHelper.onDestroy();
     }
 
 }

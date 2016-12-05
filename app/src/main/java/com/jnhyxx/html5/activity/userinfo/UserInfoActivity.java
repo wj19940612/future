c2c2fc493171214161afc3afb967c5f4864a0a6b
wangjie <1291360102@qq.com>
2016-12-05 14:32:02 +0800

    增加个人信息界面

---
+++ app/src/main/java/com/jnhyxx/html5/activity/userinfo/UserInfoActivity.java
@@ -0,0 +1,18 @@
+package com.jnhyxx.html5.activity.userinfo;
+
+import android.os.Bundle;
+
+import com.jnhyxx.html5.R;
+import com.jnhyxx.html5.activity.BaseActivity;
+
+/**
+ * 用户个人信息界面
+ */
+public class UserInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+    }
+}

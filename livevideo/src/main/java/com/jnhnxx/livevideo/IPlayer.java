15a3002495cada48ff477612f8339d19acdae9b9
John Zhang <zxukai@gmail.com>
2016-12-16 17:13:49 +0800

    重构播放器代码

--- livevideo/src/main/java/com/jnhnxx/livevideo/IPlayer.java
+++ livevideo/src/main/java/com/jnhnxx/livevideo/IPlayer.java
@@ -1,12 +1,17 @@
 package com.jnhnxx.livevideo;
 
-public interface ILivePlayer {
+public interface IPlayer {
+
+    int IDLE = 0;
+    int INITIALIZED = 1;
+    int PREPARING = 2;
+    int PREPARED = 3;
+    int STARTED = 4;
+    int ERROR = -1;
 
     void start();
 
     void stop();
-
-    boolean isStopped();
 
     boolean isStarted();
 

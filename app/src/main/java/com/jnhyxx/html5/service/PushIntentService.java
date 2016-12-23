0873a1d5ce8fc87cd7a38e12989e739955090f82
wangjie <1291360102@qq.com>
2016-12-23 18:12:42 +0800

    删除2.9.3.0 个推，使用最新的2.9.5.0，改用service

--- app/src/main/java/com/jnhyxx/html5/service/PushIntentService.java
+++ app/src/main/java/com/jnhyxx/html5/service/PushIntentService.java
@@ -4,9 +4,22 @@ package com.jnhyxx.html5.service;
  * Created by ${wangJie} on 2016/12/23.
  */
 
+import android.app.ActivityManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.igexin.sdk.GTIntentService;
 import com.igexin.sdk.PushConsts;
 import com.igexin.sdk.PushManager;
@@ -14,6 +27,12 @@ import com.igexin.sdk.message.FeedbackCmdMessage;
 import com.igexin.sdk.message.GTCmdMessage;
 import com.igexin.sdk.message.GTTransmitMessage;
 import com.igexin.sdk.message.SetTagCmdMessage;
+import com.jnhyxx.html5.R;
+import com.jnhyxx.html5.activity.MainActivity;
+import com.jnhyxx.html5.activity.account.MessageCenterListItemInfoActivity;
+import com.jnhyxx.html5.domain.msg.SysMessage;
+import com.johnz.kutils.DateUtil;
+import com.johnz.kutils.Launcher;
 
 /**
  * 继承 GTIntentService 接收来自个推的消息, 所有消息在线程中回调, 如果注册了该服务, 则务必要在 AndroidManifest中声明, 否则无法接受消息<br>
@@ -25,6 +44,10 @@ import com.igexin.sdk.message.SetTagCmdMessage;
 public class PushIntentService extends GTIntentService {
 
     private static final String TAG = "GetuiSdkDemo";
+
+    public static final String PUSH_ACTION = "com.jnhyxx.html5.service.PushIntentService";
+
+    public static final String KEY_PUSH_DATA = "PUSH_DATA";
 
     /**
      * 为了观察透传数据变化.
@@ -61,12 +84,7 @@ public class PushIntentService extends GTIntentService {
         } else {
             String data = new String(payload);
             Log.d(TAG, "receiver payload = " + data);
-
-            // 测试消息为了观察数据变化
-            if (data.equals("收到一条透传测试消息")) {
-                data = data + "-" + cnt;
-                cnt++;
-            }
+            handleMessage(context, data);
         }
 
         Log.d(TAG, "----------------------------------------------------------------------------------------------");
@@ -149,6 +167,22 @@ public class PushIntentService extends GTIntentService {
         Log.d(TAG, "settag result sn = " + sn + ", code = " + code + ", text = " + text);
     }
 
+    private void handleMessage(Context context, String data) {
+        try {
+            Log.d(TAG, "===data  " + data);
+            SysMessage sysMessage = new Gson().fromJson(data, SysMessage.class);
+            if (mainIsTopActivity(context) && sysMessage.getPushSendType() != SysMessage.PUSH_SYS_TYPE_PUSH) {
+                LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(PUSH_ACTION).putExtra(KEY_PUSH_DATA, sysMessage));
+            } else {
+                Intent messageIntent = setPendingIntent(context, data);
+                PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, messageIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+                createNotification(context, pendingIntent, data);
+            }
+        } catch (JsonSyntaxException e) {
+            Log.d(TAG, "  " + e.getCause());
+        }
+    }
+
     private void feedbackResult(FeedbackCmdMessage feedbackCmdMsg) {
         String appid = feedbackCmdMsg.getAppid();
         String taskid = feedbackCmdMsg.getTaskId();
@@ -159,6 +193,41 @@ public class PushIntentService extends GTIntentService {
 
         Log.d(TAG, "onReceiveCommandResult -> " + "appid = " + appid + "\ntaskid = " + taskid + "\nactionid = " + actionid + "\nresult = " + result
                 + "\ncid = " + cid + "\ntimestamp = " + timestamp);
+    }
+
+    private void createNotification(Context context, PendingIntent pendingIntent, String data) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        SysMessage sysMessage = new Gson().fromJson(data, SysMessage.class);
+        builder.setContentTitle(sysMessage.getPushTopic());
+        builder.setContentText(sysMessage.getPushContent());
+        builder.setContentIntent(pendingIntent);
+        builder.setWhen(DateUtil.getStringToDate(sysMessage.getCreateTime()));
+        builder.setAutoCancel(true);
+
+        BitmapDrawable drawable = (BitmapDrawable) ContextCompat.getDrawable(context, R.mipmap.ic_launcher);
+        Bitmap bitmap = drawable.getBitmap();
+        builder.setLargeIcon(bitmap);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setDefaults(NotificationCompat.DEFAULT_ALL);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(R.string.app_name, builder.build());
+    }
+
+    @NonNull
+    private Intent setPendingIntent(Context context, String data) {
+        Intent messageIntent = new Intent(context, MessageCenterListItemInfoActivity.class);
+        SysMessage sysMessage = new Gson().fromJson(data, SysMessage.class);
+        messageIntent.putExtra(Launcher.EX_PAYLOAD, sysMessage);
+        return messageIntent;
+    }
+
+    private boolean mainIsTopActivity(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        ComponentName topActivity = activityManager.getRunningTasks(1).get(0).topActivity;
+        Log.d(TAG, "上层的Activity " + topActivity.getClassName());
+        MainActivity mainActivity = new MainActivity();
+        Log.d(TAG, "MainActivity  " + mainActivity.getClass().getName());
+        return topActivity.getClassName().contains(mainActivity.getClass().getName());
     }
 
 }

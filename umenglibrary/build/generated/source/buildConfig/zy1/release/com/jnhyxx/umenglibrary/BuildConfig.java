831bbe1161550a06e73a56222c18ae887a60cbc7
wangjie <1291360102@qq.com>
2017-01-17 12:03:25 +0800

    更新sdk

---
+++ umenglibrary/build/generated/source/buildConfig/zy1/release/com/jnhyxx/umenglibrary/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jnhyxx.umenglibrary;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.jnhyxx.umenglibrary";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "zy1";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}

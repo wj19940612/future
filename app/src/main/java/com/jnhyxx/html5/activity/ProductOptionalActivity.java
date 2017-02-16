6a73f9dc913144abdcb6174208786fd2c0ec9f61
yangguangda <yangguangda@hashxy.com>
2017-02-16 18:51:04 +0800

    增加品种自选界面

---
+++ app/src/main/java/com/jnhyxx/html5/activity/ProductOptionalActivity.java
@@ -0,0 +1,17 @@
+package com.jnhyxx.html5.activity;
+
+import android.os.Bundle;
+
+import com.jnhyxx.html5.R;
+
+/**
+ * Created by MR.YANG on 2017/2/16.
+ */
+
+public class ProductOptionalActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_optional);
+    }
+}

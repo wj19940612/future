5f3f9cd894e18a81797882f52201ef42c28dfc40
yangguangda <yangguangda@hashxy.com>
2017-02-22 18:31:12 +0800

    解决webView内存泄漏问题

--- app/src/main/java/com/jnhyxx/html5/activity/WebViewActivity.java
+++ app/src/main/java/com/jnhyxx/html5/activity/WebViewActivity.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.DownloadListener;
@@ -97,7 +99,28 @@ public class WebViewActivity extends BaseActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mWebView.destroy();
+        destroy();
+    }
+
+    public void destroy() {
+        if (mWebView != null) {
+            // 5.1 如果先调用destroy()方法，则会命中if (isDestroyed()) return;这一行代码，需要先onDetachedFromWindow()，再
+            // destory()
+            ViewParent parent = mWebView.getParent();
+            if (parent != null) {
+                ((ViewGroup) parent).removeView(mWebView);
+            }
+            mWebView.stopLoading();
+            // 退出时调用此方法，移除绑定的服务，否则某些特定系统会报错
+            mWebView.getSettings().setJavaScriptEnabled(false);
+            mWebView.clearHistory();
+            mWebView.clearView();
+            mWebView.removeAllViews();
+            try {
+                mWebView.destroy();
+            } catch (Throwable ex) {
+            }
+        }
     }
 
     @Override
